10f69ecdf2610b30b9d745b58851081be34a8759
JD <49376309+joyjitdas10@users.noreply.github.com>
2020-10-29 10:12:30 +0530

    Updates

---
+++ precache-manifest.6a956cd07d6b6cd4e63ef3d71b64b349.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba01caa499624bebee400a66e3243a81",
+    "url": "/Websiteone/index.html"
+  },
+  {
+    "revision": "63dcb7bed1e9421b3384",
+    "url": "/Websiteone/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "aa5861e7784496fa6370",
+    "url": "/Websiteone/static/css/main.26b08fe0.chunk.css"
+  },
+  {
+    "revision": "63dcb7bed1e9421b3384",
+    "url": "/Websiteone/static/js/2.55ee8029.chunk.js"
+  },
+  {
+    "revision": "58d64a5bc3b06f2933294a2521985f38",
+    "url": "/Websiteone/static/js/2.55ee8029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa5861e7784496fa6370",
+    "url": "/Websiteone/static/js/main.7de255ea.chunk.js"
+  },
+  {
+    "revision": "200f6fcbf2cf0a80608d",
+    "url": "/Websiteone/static/js/runtime-main.ccb8ae48.js"
+  }
+]);
